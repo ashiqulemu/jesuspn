942a4645b2f4f3f704683ec282e558a38752ebd2
ashiqulemu.jpi@gmail.com <ashiqulemu.jpi@gmail.com>
2018-12-06 12:43:39 +0600

    first

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state:{
+        message:'this is vuex message'
+    },
+    mutations:{
+
+    },
+    action:{
+
+    },
+})
+export default store;
