6254a48b8e912d0311bb295fcc02abac376a10e2
shahanursharif <sharif@microdreamit.com>
2018-12-08 13:25:45 +0600

    first

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -1,6 +1,37 @@
 <template>
-    <div>
-        footer
+    <div class="container">
+       <div class="row">
+           <div class="col-md-4">
+               <h1>LOGO</h1>
+               <p>
+                   We are an online marketplace platform for booking social group trips.
+                   Our service is more affordable and convenient than hotel.
+               </p>
+           </div>
+           <div class="col-md-4">
+              <div class="row">
+                  <div class="col">
+                      <h5>USEFUL LINKS</h5>
+                      <p>About Us</p>
+                      <p>Help ?</p>
+                      <p>Contact Us</p>
+                  </div>
+                  <div class="col">
+                      <h5 class="hide"></h5>
+                      <p>Terms Of Use</p>
+                      <p>Privacy Policy</p>
+                      <p>Copyright Policy</p>
+                  </div>
+              </div>
+           </div>
+           <div class="col-md-4">
+               <h5>LANGUAGE</h5>
+               <select name="" id="" class="form-control">
+                   <option value="engine">English</option>
+                   <option value="spanish">Spanish</option>
+               </select>
+           </div>
+       </div>
     </div>
 </template>
 
