0e74c48d556d9401830a5197d60657494f33dd4e
shahanursharif <sharif@microdreamit.com>
2018-12-08 16:34:17 +0600

    first

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -4,7 +4,11 @@
             <div class="container-fluid">
                 <nav class="navbar navbar-expand-md navbar-dark">
                     <a class="navbar-brand" href="#">logo here</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarsExample04"
+                            aria-controls="navbarsExample04" aria-expanded="false"
+                            aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
