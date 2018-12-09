ae6a7d2310474a8cbb218127ecf1d9f15e00944a
shahanursharif <sharif@microdreamit.com>
2018-12-09 17:53:43 +0600

    first

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -18,11 +18,54 @@
                             test
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                            <div class="form-group">
+                                <label >Check In</label>
+                                <datepicker :input-class="'form-control'" ></datepicker>
+                            </div>
+                            <div class="form-group">
+                                <label >Check Out</label>
+                                <datepicker :input-class="'form-control'" ></datepicker>
+                            </div>
+                            <div class="form-group">
+                                <label for="room">Room</label>
+                                <select name="room" id="room" class="form-control">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="room">Type</label>
+                                <select name="type" id="type" class="form-control">
+                                    <option value="small">Small Room</option>
+                                    <option value="medium">Medium Room</option>
+                                    <option value="large">Large Room</option>
+                                </select>
+                            </div>
+
+                            <div class="list-group-flush">
+                                <div class="list-group-item">
+                                    <div>
+                                        <span>Room 1 Small 100€*2 nights</span>
+                                        <span class="float-right">200€</span>
+                                    </div>
+                                </div>
+                                <div class="list-group-item">
+                                    <span>Discount(0%)</span>
+                                    <span class="float-right">0.00€</span>
+                                </div>
+                                <div class="list-group-item">
+                                    <span>Total</span>
+                                    <span class=" float-right text-primary">200€</span>
+                                </div>
+                                <div class="form-group text-center">
+                                    <br>
+                                    <button class="btn btn btn-primary">BOOK NOW!</button>
+                                </div>
+                                <div class="from-group text-center">
+                                    <a href="#" >You won't be charged yet</a>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -32,8 +75,10 @@
 </template>
 
 <script>
+    import Datepicker from 'vuejs-datepicker'
     export default {
-        name: "experience"
+        name: "experience",
+        components:{Datepicker}
     }
 </script>
 
