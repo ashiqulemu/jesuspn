d9a5f5382ce542f803da9942b136418d6a3bbcba
shahanursharif <sharif@microdreamit.com>
2018-12-12 18:44:17 +0600

    working on experience page, and trying to solve flaticon on compiling.

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -146,7 +146,23 @@
                         <div class="card-body">
                             <h4>Host & Location</h4>
                             <br>
-                            <h1>Map</h1>
+                            <div class="blocation">
+                                <h3>Location <a href="#" title="">Get Direction</a></h3>
+                                <div class="contact-map">
+                                    <div id="map_div">&nbsp;</div>
+                                </div>
+                                <span>Collins Street West, Victoria 8007, Australia.</span>
+                                <span>+1 246-345-0695</span>
+                                <span>info@example.com</span>
+                                <div class="wsocial">
+                                    <a href="#" title=""><i class="fa fa-facebook"></i></a>
+                                    <a href="#" title=""><i class="fa fa-twitter"></i></a>
+                                    <a href="#" title=""><i class="fa fa-linkedin"></i></a>
+                                    <a href="#" title=""><i class="fa fa-pinterest"></i></a>
+                                    <a href="#" title=""><i class="fa fa-google"></i></a>
+                                    <a href="#" title=""><i class="fa fa-dribbble"></i></a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <br>
@@ -270,9 +286,44 @@
 
 <script>
     import Datepicker from 'vuejs-datepicker'
+
     export default {
         name: "experience",
-        components:{Datepicker}
+        components:{Datepicker},
+        mounted(){
+            console.log($);
+            // $(document).on('ready', function(){
+            //
+            //     //------- Google Maps ---------//
+            //
+            //     // Creating a LatLng object containing the coordinate for the center of the map
+            //     var latlng = new google.maps.LatLng(53.385846,-1.471385);
+            //
+            //     // Creating an object literal containing the properties we want to pass to the map
+            //     var options = {
+            //         zoom: 15, // This number can be set to define the initial zoom level of the map
+            //         center: latlng,
+            //         mapTypeId: google.maps.MapTypeId.ROADMAP // This value can be set to define the map type ROADMAP/SATELLITE/HYBRID/TERRAIN
+            //     };
+            //     // Calling the constructor, thereby initializing the map
+            //     var map = new google.maps.Map(document.getElementById('map_div'), options);
+            //
+            //     // Define Marker properties
+            //     var image = new google.maps.MarkerImage('images/pincontact.png',
+            //         new google.maps.Size(100, 100),
+            //         new google.maps.Point(0,0),
+            //         new google.maps.Point(18, 42)
+            //     );
+            //
+            //     // Add Marker
+            //     var marker1 = new google.maps.Marker({
+            //         position: new google.maps.LatLng(53.385846,-1.471385),
+            //         map: map,
+            //         icon: image // This path is the custom pin to be shown. Remove this line and the proceeding comma to use default pin
+            //     });
+            //
+            // });
+        }
     }
 </script>
 
